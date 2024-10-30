98325346db897ef1cd593ccf2b7a861a17a8922b
Anton Kokarev <anton@kokarev.su>
2024-10-31 01:18:00 +0300

    add module version

---
+++ config/initializers/version.rb
@@ -0,0 +1,12 @@
+module TestGuru
+  MAJOR	= 0
+  MINOR	= 1
+  TINY	= 0
+  BUILD = `git rev-list --count HEAD`.strip.rjust(4, '0').freeze
+
+  VERSION = [MAJOR, MINOR, TINY, BUILD].compact.join(".").freeze
+
+  def self.current_version
+    VERSION
+  end
+end
